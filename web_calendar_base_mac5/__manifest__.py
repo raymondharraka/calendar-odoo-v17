82dc08b13af539bb14ea47fd27e989e67fabf6ba
Raymond <45762422+raymondharraka@users.noreply.github.com>
2025-06-04 23:19:36 -0400

    Add files via upload

---
+++ web_calendar_base_mac5/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Calendar Base',
+    'version': '17.0.1.2',
+    'summary': 'Base Module for Calendar Customizations',
+    'description': """
+Calendar Base
+=============
+
+Base module for calendar customizations.
+""",
+    'category': 'Hidden',
+    'author': 'MAC5',
+    'contributors': ['MAC5'],
+    'website': 'https://apps.odoo.com/apps/modules/browse?author=MAC5',
+    'depends': ['calendar'],
+    'data': [
+        'views/calendar_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'images': [],
+    'price': 0.00,
+    'currency': 'EUR',
+    'support': 'mac5_odoo@outlook.com',
+    'license': 'OPL-1',
+}
